ca2ec94b0cf3433ad06ad781afdc2ccc225fbebf
Jasmine Wang <jasminewang76@gmail.com>
2020-01-10 21:48:18 -0500

    skeleton code for when api is ready

--- amazon_api2.py
+++ amazon_api2.py
@@ -6,12 +6,18 @@ I downloaded the source files, and ran setup.py install myself after using 2to3 
 from amazonproduct import API
 api = API(locale='us')
 
-def main():
+def get_book_info(ASIN):
   # get all books from result set and
   # print author and title
-  for book in api.item_search("Books", Publisher="Galileo Press"):
-      print('%s: "%s"' % (book.ItemAttributes.Author,
-                          book.ItemAttributes.Title))
+
+  # Example single item lookup
+  book = api.call(Operation='ItemLookup', ItemId='B00008OE6I', )
+  return book.ItemAttributes.Author, book.ItemAttributes.Title, \
+    book.ItemAttributes.Reviews, book.ItemAttributes.OfferSummary
+
 
 if __name__ == '__main__':
-  main()
+  for book in api.item_search("Books", Publisher="Galileo Press"):
+    print('%s: "%s"' % (book.ItemAttributes.Author,
+                        book.ItemAttributes.Title))
+
